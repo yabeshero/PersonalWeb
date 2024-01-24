c9b6f595f875da49914711766b0999707e72c159
yabeshero <157552252+yabeshero@users.noreply.github.com>
2024-01-24 19:39:49 +0800

    Add files via upload

---
+++ placeorder.php
@@ -0,0 +1,8 @@
+<?=template_header('Place Order')?>
+
+<div class="placeorder content-wrapper">
+    <h1>Pesanan Anda Telah kami Terima</h1>
+    <p>Terima kasih telah berlangganan dengan kami! Stay tune terus email Anda untuk mengecek Pesanan Anda.</p>
+</div>
+
+<?=template_footer()?>
